491912d97d8c06e719103510371316edbe99b108
Ivan Hernandez <ivan.hernandez.coral@gmail.com>
2020-07-06 13:45:21 -0500

    V6

--- src/main/java/com/ihc/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/ihc/apirest/controllers/ClienteRestController.java
@@ -81,12 +81,12 @@ public class ClienteRestController
      * @param cedula, Cédula con la cual se buscara el cliente en BD
      * @return Cliente encontrado
      */
-    @GetMapping(value = "/covid/{cedula}/{idTienda}")
-    public ResponseEntity<Cliente> validarClienteByCedulaAndIdTienda(@PathVariable String cedula, @PathVariable Long idTienda) 
+    @GetMapping(value = "/covid/{cedula}")
+    public ResponseEntity<Cliente> findByCedula(@PathVariable String cedula) 
     {
         try
         {
-            Cliente cliente = clienteRepository.validarClienteByCedulaAndIdTienda(cedula, idTienda);
+            Cliente cliente = clienteRepository.findByCedula(cedula);
 
             if(null != cliente)
             {
